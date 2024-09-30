fd6e86be5b50c524f84c994adaecccb8de433f9f
Dee S <dsmallwood0427@gmail.com>
2024-09-30 10:51:01 -0500

    first commit

---
+++ src/main/java/com/pluralsight/Calculator.java
@@ -0,0 +1,42 @@
+package com.pluralsight;
+
+class Calculator{
+
+   public static void main(String[] args){
+     // Invoking or calling a method (static)
+     Calculator calculator = new Calculator();
+     calculator.addInstance(1,2);
+     addStatic(3,4);
+
+
+   }
+
+public void addInstance(int num1, int num2){
+    System.out.println(num1 + num2);
+}
+
+public static void addStatic(int num1, int num2){
+    System.out.println(num1 + num2);
+
+}
+// Static add
+// static subtract
+// Static multiplication
+// Static division
+
+    addStatic(num1:45, num2:89)
+    public static void addStatic(int num1, int num2) {
+        System.out.println(num1 + num2)
+    }
+
+    public static void
+
+// Receive two inputs (ints)
+// Return a single number (int, double)
+
+// Extras
+// Static Mod
+// Three inputs
+
+
+}
